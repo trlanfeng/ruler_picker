62e5120320bece937e5cd273912baf68594fdbd9
trlanfeng <trlanfeng@foxmail.com>
2020-03-17 13:33:46 +0800

    Clear the test file

--- test/ruler_picker_test.dart
+++ test/ruler_picker_test.dart
@@ -4,10 +4,6 @@ import 'package:ruler_picker/ruler_picker.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+
   });
 }
